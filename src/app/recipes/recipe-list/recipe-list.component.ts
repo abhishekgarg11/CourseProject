04f998789b29474cd52996ae70ee20d6f4109fac
Abhishek <abhishekgarg236@gmail.com>
2019-09-16 21:17:36 +0530

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import{Recipe} from '../recipe.model'
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+new Recipe('A Test Recipe','This is a Test Description','https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/08/old-delhi-style-butter-chicken.jpg')
+
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
