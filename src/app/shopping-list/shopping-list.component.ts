1813fffa17bfe1e504715d0caf33cb787a34ccfd
Abhishek <abhishekgarg236@gmail.com>
2019-09-29 11:43:53 +0530

    Shopping List Edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {Ingredient} from '../shared/ingredient.model'
+import { Ingredient } from '../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-ingredients :Ingredient[]=[
-  new Ingredient('Apples',5),
-  new Ingredient('Oranges',10),
-];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges', 10),
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
